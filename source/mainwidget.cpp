aae03bab17356dad9e532354091a7fc27d7fd809
emigdioz <emigdioz@gmail.com>
2016-07-01 23:15:09 -0700

    Update UI

--- source/mainwidget.cpp
+++ source/mainwidget.cpp
@@ -10,6 +10,11 @@ MainWidget::MainWidget(QWidget *parent) :
 	QFontDatabase::addApplicationFont(":/fonts/resources/fonts/Lato-Medium.ttf");
 	QFontDatabase::addApplicationFont(":/fonts/resources/fonts/Roboto-Light.ttf");
 	QFontDatabase::addApplicationFont(":/fonts/resources/fonts/Roboto-Medium.ttf");
+	connect(ui->listWidget, SIGNAL(currentRowChanged(int)), ui->stackedWidget, SLOT(setCurrentIndex(int)));
+	connect(ui->listWidgetData, SIGNAL(currentRowChanged(int)), ui->stackedWidgetData, SLOT(setCurrentIndex(int)));
+	connect(ui->listWidgetSetup, SIGNAL(currentRowChanged(int)), ui->stackedWidgetSetup, SLOT(setCurrentIndex(int)));
+	connect(ui->listWidgetGP, SIGNAL(currentRowChanged(int)), ui->stackedWidgetGP, SLOT(setCurrentIndex(int)));
+	connect(ui->listWidgetResults, SIGNAL(currentRowChanged(int)), ui->stackedWidgetResults, SLOT(setCurrentIndex(int)));
 }
 
 MainWidget::~MainWidget()

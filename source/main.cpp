ab73bb27f663f0e893aa13a2b18652b317e35a55
emigdioz <emigdioz@gmail.com>
2017-04-06 12:49:52 -0700

    Minor change

--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,19 @@
 #include "mainwidget.h"
 #include <QApplication>
 #include <QSplashScreen>
+#include <QDesktopWidget>
 #include <QTimer>
 
-int main(int argc, char *argv[])
-{
-	QApplication a(argc, argv);
-    //QThread::currentThread()->setPriority(QThread::HighPriority);
-    QPixmap pixmap(":/icons/resources/images/splash.png");
-    QSplashScreen splash(pixmap);
-    splash.show();
-    //a.processEvents(QEventLoop::AllEvents);
-
-    MainWidget w;
-    QTimer::singleShot(2500,&splash,SLOT(close()));
-    QTimer::singleShot(2500,&w,SLOT(show()));
-    //w.show();
-    //splash.finish(&w);
-	return a.exec();
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  QThread::currentThread()->setPriority(QThread::HighPriority);
+  QPixmap pixmap(":/icons/resources/images/splash.png");
+  QSplashScreen splash(pixmap);
+  splash.show();
+  MainWidget w;
+  w.move(QApplication::desktop()->screen()->rect().center() -
+         w.rect().center());
+  QTimer::singleShot(3000, &splash, SLOT(close()));
+  QTimer::singleShot(3000, &w, SLOT(show()));
+  return a.exec();
 }
